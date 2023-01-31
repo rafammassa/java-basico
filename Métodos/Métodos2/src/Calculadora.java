44af3b6a01f756a8416ba33e8c5f52788fa20382
rafammassa <rafaella_massa@hotmail.com>
2023-01-31 09:29:24 -0300

    Métodos Calculadora

---
+++ Métodos/Métodos2/src/Calculadora.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void soma (double num1, double num2) {
+        double resultado = num1 + num2;
+        System.out.println("A soma dos valores é igual a " + resultado);
+    }
+
+    public static void subtracao (double num1, double num2){
+        double resultado = num1 - num2;
+        System.out.println("A subtração dos valores é igual a " + resultado);
+    }
+
+    public static void multiplicacao (double num1, double num2) {
+        double resultado = num1 * num2;
+        System.out.println("A multiplicação dos valores é igual a " + resultado);
+    }
+
+    public static void divisao (double num1, double num2) {
+        double resultado = num1 / num2;
+        System.out.println("A divisão dos valores é igual a " + resultado);
+    }
+}
