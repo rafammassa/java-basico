ccfb04f08c10c17413064f655a5e4c9c2263f431
rafammassa <rafaella_massa@hotmail.com>
2023-02-06 14:20:09 -0300

    Continuação do exercício 1 dos métodos de ArrayList

--- Collections/Exercicio1/src/ExemploList.java
+++ Collections/Exercicio1/src/ExemploList.java
@@ -1,5 +1,7 @@
 import java.util.List;
-import java.util.ArrayList;;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;;
 
 public class ExemploList {
     public static void main(String[] args) throws Exception {
@@ -35,5 +37,41 @@ public class ExemploList {
         System.out.println("Exiba todas as notas na ordem que foram informadas: ");
         for (Double nota : notas) System.out.println(nota);
 
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator(); //realizando a soma dos valores
+        Double soma = 0d;
+        while (iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota 0.0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova a nota na posição 0: ");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7.0 e exiba a lista: ");
+        Iterator<Double> menor7 = notas.iterator();
+        while (menor7.hasNext()){
+            Double next = menor7.next();
+            if (next < 7) menor7.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista: ");
+        notas.clear();
+        System.out.println(notas);
+        System.out.println("A lista está vazia? " + notas.isEmpty());
     }
 }
